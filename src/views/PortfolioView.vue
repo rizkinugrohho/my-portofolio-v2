352564297014d92fed94c3d7de383490a68b8900
A A S <rizkynhg@gmail.com>
2024-03-13 15:04:10 +0700

    Update Portfolio

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -6,6 +6,15 @@ export default {
       items: [
         {
           id: 1,
+          name: 'Laravel-Vue E-Commerce',
+          imageUrl: 'laravel-vue',
+          status: 'E-commerce application built with Laravel, Vue.js, Tailwind.css and Alpine.js.',
+          tech: 'Laravel, Vue Js, Tailwind, Alphine Js, Html, Css',
+          github: 'https://github.com/rizkinugrohho/laravel-vue',
+          demo: 'https://github.com/rizkinugrohho/laravel-vue'
+        },
+        {
+          id: 2,
           name: 'Nike Landing Page',
           imageUrl: 'landingpage',
           status: 'Discover stylish Nike arrivals, quality comfort, and innovation for your active life.',
@@ -14,7 +23,7 @@ export default {
           demo: 'https://rizkinugrohho.github.io/nike-landing-page/'
         },
         {
-          id: 2,
+          id: 3,
           name: 'Booking Hotel Web',
           imageUrl: 'portfolio-hotel',
           status: 'A Simple Hotel Booking Website. This project aims to create a user-friendly platform for booking hotel accommodations.',
@@ -23,7 +32,7 @@ export default {
           demo: 'null'
         },
         {
-          id: 3,
+          id: 4,
           name: 'Personal Website',
           imageUrl: 'portfolio_v2',
           status: 'My personal website, I created this website to display my profile, skills and projects. As well as my place to try new technology.',
@@ -32,7 +41,7 @@ export default {
           demo: 'rizkinugroho.netlify.app'
         },
         {
-          id: 4,
+          id: 5,
           name: 'Weather App',
           imageUrl: 'weatherapp',
           status: 'Android Mobile Application that shows todays weather and weather forecast for the next 5 days in various cities and countries.',
@@ -41,7 +50,7 @@ export default {
           demo: 'https://github.com/rizkinugrohho/MobileApplication_Weather-App'
         },
         {
-          id: 5,
+          id: 6,
           name: 'Mandiri News App',
           imageUrl: 'mandiri',
           status: 'Challenge from Bank Mandiri - Creating an Android Mobile Application that displays a list of news using API from https://newsapi.org',
@@ -50,7 +59,7 @@ export default {
           demo: 'https://github.com/rizkinugrohho/MobileApplication_MandiriNews'
         },
         {
-          id: 6,
+          id: 7,
           name: 'Progresive Web App React Js',
           imageUrl: 'webapp',
           status: 'a progressive e-commerce web application, which has Push Notification Features, App Banner (Add to Home Screen), Splash screen on the application, Service Worker with Workbox JS.',
@@ -59,7 +68,7 @@ export default {
           demo: 'https://web-apps-phi.vercel.app/'
         },
         {
-          id: 7,
+          id: 8,
           name: 'Bookshelf App',
           imageUrl: 'bookshelf',
           status: 'allows users to search for books, view book details, leave reviews, add books to favorites, and view example categories of books.',
