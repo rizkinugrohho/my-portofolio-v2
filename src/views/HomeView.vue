22b40d31fc375377cb5843f3ee5c8694ed30d7dc
Rizki Nugroho <rizkynhg@gmail.com>
2024-05-09 21:05:55 +0700

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,7 +26,7 @@
         </svg>Download Resume</button> -->
     </div>
     <div class="flex justify-center md:justify-start fadein-right"><img alt="avatar" fetchpriority="high" width="300" height="300" decoding="async" data-nimg="1"
-        class="w-10/12 md:h-auto rounded-full border-4 border-sky-200 pict" src="https://i.ibb.co/7yVLPPd/Safeimagekit-resized-img.png">
+        class="w-10/12 md:h-auto rounded-full border-4 border-sky-200 pict" src="https://i.imgur.com/KhHFHvB.jpeg">
     </div>
   </main>
 </template>
